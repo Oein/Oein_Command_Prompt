eff5ebd495215ec07afedd7e3cf2ca46d21129e2
Oein <62917247+Oein@users.noreply.github.com>
2020-09-12 15:36:30 +0900

    add- bash command

--- Oein_Command_Prompt/Command.hpp
+++ Oein_Command_Prompt/Command.hpp
@@ -29,6 +29,23 @@ void input_command(string command_string) {
 
 		cout << out;
 	}
+
+	if (commands[0] == "bash") {
+		if (commands.size() < 2) return;
+		ifstream in(directory + commands[1]);
+
+		if (in.is_open() == true) {
+			char buffer[10000];
+
+			while (in) {
+				in.getline(buffer, 10000);
+				input_command(buffer);
+			}
+		}
+		else {
+			cout << "Oops! Error! \nThere is no file!";
+		}
+	}
 }
 
 void on_start() {
