1b80b662c341c8964d73bc8db44153ba69b8bbb9
Oein <62917247+Oein@users.noreply.github.com>
2020-09-12 16:34:57 +0900

    Change funtion to funtions

--- Oein_Command_Prompt/Command.hpp
+++ Oein_Command_Prompt/Command.hpp
@@ -12,6 +12,34 @@ using namespace std;
 string directory = "/";
 string username = "User";
 
+void echo(vector<string> commands) {
+	if (commands.size() < 2) return;
+	string out = "";
+
+	for (int i = 1; i < commands.size(); i++) {
+		out = out + commands[i] + " ";
+	}
+
+	cout << out;
+}
+
+void bash(vector<string> commands){
+	if (commands.size() < 2) return;
+	ifstream in(directory + commands[1]);
+
+	if (in.is_open() == true) {
+		char buffer[10000];
+
+		while (in) {
+			in.getline(buffer, 10000);
+			input_command(buffer);
+		}
+	}
+	else {
+		cout << "Oops! Error! \nThere is no file!";
+	}
+}
+
 void input_command(string command_string) {
 	command_string = replace_all(command_string, "%username%", username);
 
@@ -19,33 +47,8 @@ void input_command(string command_string) {
 
 	if (commands.size() < 1) return;
 
-	if (commands[0] == "echo") {
-		if (commands.size() < 2) return;
-		string out = "";
-
-		for (int i = 1; i < commands.size(); i++) {
-			out = out + commands[i] + " ";
-		}
-
-		cout << out;
-	}
-
-	if (commands[0] == "bash") {
-		if (commands.size() < 2) return;
-		ifstream in(directory + commands[1]);
-
-		if (in.is_open() == true) {
-			char buffer[10000];
-
-			while (in) {
-				in.getline(buffer, 10000);
-				input_command(buffer);
-			}
-		}
-		else {
-			cout << "Oops! Error! \nThere is no file!";
-		}
-	}
+	if (commands[0] == "echo") { echo(commands); }
+	if (commands[0] == "bash") { bash(commands); }
 }
 
 void on_start() {
