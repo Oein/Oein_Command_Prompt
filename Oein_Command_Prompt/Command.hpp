d304730c26dbecbff9df06639c5d318c953e70c9
Oein <62917247+Oein@users.noreply.github.com>
2020-09-12 15:28:56 +0900

    add- echo command

--- Oein_Command_Prompt/Command.hpp
+++ Oein_Command_Prompt/Command.hpp
@@ -18,6 +18,17 @@ void input_command(string command_string) {
 	vector<string> commands = split(command_string);
 
 	if (commands.size() < 1) return;
+
+	if (commands[0] == "echo") {
+		if (commands.size() < 2) return;
+		string out = "";
+
+		for (int i = 1; i < commands.size(); i++) {
+			out = out + commands[i] + " ";
+		}
+
+		cout << out;
+	}
 }
 
 void on_start() {
